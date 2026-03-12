06e2d09ea29a461b282d83b357f41dc7344154a5
vaainci <davidpinto051007@gmail.com>
2026-03-12 15:06:43 +0100

    Remove texCoord from Vertex struct
    Remove the glm::vec2 texCoord member from the Vertex struct in HammerEngine.h. This change drops per-vertex texture coordinates and may require updating vertex input attribute descriptions, pipeline / shader code, and any codepaths that previously accessed vertex.texCoord.

--- include/HammerEngine/HammerEngine.h
+++ include/HammerEngine/HammerEngine.h
@@ -26,7 +26,6 @@
 struct Vertex {
     glm::vec3 pos;
     glm::vec3 color;
-    glm::vec2 texCoord;
 
     static VkVertexInputBindingDescription getBindingDescription();
     static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions();
